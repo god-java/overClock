188751bcd5d49077beff7f0c35de1f41f3f39884
jun <gkdlwkqk@gmail.com>
2018-09-28 21:32:40 +0900

    first

---
+++ overClock/src/main/java/com/level/clock/OverClockApplication.java
@@ -0,0 +1,15 @@
+package com.level.clock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class OverClockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverClockApplication.class, args);
+	}
+}
