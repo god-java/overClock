7a0cd53b1227c05ded119e85a1eecee62acd677e
jun <gkdlwkqk@gmail.com>
2018-09-28 21:37:05 +0900

    dfff

---
+++ eeeeeee/src/main/java/com/level/clock/EeeeeeeApplication.java
@@ -0,0 +1,12 @@
+package com.level.clock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeeeeeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EeeeeeeApplication.class, args);
+	}
+}
